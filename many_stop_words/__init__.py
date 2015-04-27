a8de4c1ff8a227d7e5aee6e2822c062b2e3c2b5c
David Maze <dmaze@diffeo.com>
2015-04-27 12:04:24 -0400

    get_stop_words() returns Unicode strings.
    Fix the stopwords files to not include random U+FEFF (zero-width non-breaking
    space, the canonical byte-order mark) strewn about the files.

--- many_stop_words/__init__.py
+++ many_stop_words/__init__.py
@@ -1,11 +1,17 @@
 '''many_stop_words has lists of stop words in many languages
 
 .. This software is released under an MIT/X11 open source license.
-   Copyright 2012-2014 Diffeo, Inc.
-'''
+   Copyright 2012-2015 Diffeo, Inc.
 
+.. autofunction:: get_stop_words
+.. autodata:: available_languages
+
+'''
+from __future__ import absolute_import, division, print_function
+import codecs
 import os
 
+#: A default list of languages for :func:`get_stop_words`.
 available_languages = [
     'ar',
     'ca',
@@ -31,22 +37,26 @@ available_languages = [
     'zh',
     ]
 
+
 def get_stop_words(*lang_codes):
-    '''Returns a set of strings that are stop words for every language
-    code in `lang_codes`.  If not language codes are specified, then
-    all stop words are returned.
+    '''Get a set of words that should generally be ignored.
+
+    Returns a set of strings that are stop words for every
+    (two-letter, lowercase) language code passed as a positional
+    parameter.  If no parameters are specified, then the returned set
+    includes all languages for which data is available, listed out in
+    :data:`available_languages`.
 
     '''
     if len(lang_codes) == 0:
         lang_codes = available_languages
 
+    this_dir = os.path.dirname(__file__)
+
     stop_words = set()
     for lang_code in lang_codes:
-        for line in _open_stop_words_file(lang_code):
-            stop_words.add(line.strip())
+        path = os.path.join(this_dir, 'stopwords-{0}.txt'.format(lang_code))
+        with codecs.open(path, 'r', 'utf-8-sig') as f:
+            for line in f:
+                stop_words.add(line.strip())
     return stop_words
-
-def _open_stop_words_file(lang_code):
-    this_dir = os.path.dirname(__file__)
-    path = os.path.join(this_dir, 'stopwords-{}.txt'.format(lang_code))
-    return open(path)
--- many_stop_words/tests/test_many_stop_words.py
+++ many_stop_words/tests/test_many_stop_words.py
@@ -1,7 +1,8 @@
-
+from __future__ import absolute_import, division, print_function
 import pytest
 
 from many_stop_words import get_stop_words, available_languages
+
 
 @pytest.yield_fixture(params=available_languages)
 def lang_code(request):
@@ -12,16 +13,23 @@ def lang_code(request):
 
 def test_get_one(lang_code):
     stop_words = get_stop_words(lang_code)
+    for word in stop_words:
+        assert isinstance(word, unicode)
+        assert u'\uFEFF' not in word
     assert len(stop_words) > 0
+
 
 def test_get_two():
     stop_words = get_stop_words('en', 'it')
-    assert len(stop_words) > 0
+    assert 'been' in stop_words  # English
+    assert 'buono' in stop_words  # Italian
+    assert 'bardzo' not in stop_words  # Polish
+
 
 def test_get_all_basic():
     stop_words = get_stop_words()
-    assert len(stop_words) > 0
     assert 'if' in stop_words
+
 
 def test_get_all_equals_getting_all():
     assert get_stop_words() == get_stop_words(*available_languages)
